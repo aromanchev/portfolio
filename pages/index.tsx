bc2f5bbb1b64189552814a56e54c7d6c69969e12
aromanchev <aromanchev@businessolver.com>
2022-11-30 17:19:23 +0400

    upd meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,13 @@ export default function Home() {
           content="React Engineer, Software Engineer, JavaScript Engineer, Frontend, ReactJS"
         />
         <meta name="author" content="Artur Romanchev" />
+        <meta property="og:title" content="Artur Romanchev" />
+        <meta
+          property="og:description"
+          content="Frontend Developer, Software Engineer, Blockchain Developer."
+        />
+        <meta property="og:image" content="/portfolio-img.jpg" />
+        <meta property="og:url" content="aromanchev.com" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
