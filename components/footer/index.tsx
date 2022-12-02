9b8746747d154441867420f64e056c047b75fd34
aromanchev <aromanchev@businessolver.com>
2022-12-02 22:42:14 +0400

    upda accssebility

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -17,6 +17,7 @@ export const Footer = () => (
 
       <li>
         <Link
+          aria-label="Linkedin profile"
           href="https://www.linkedin.com/in/%D0%B0%D1%80%D1%82%D1%83%D1%80-%D1%80%D0%BE%D0%BC%D0%B0%D0%BD%D1%87%D0%B5%D0%B2-3a2287189/"
           target="_blank"
         >
